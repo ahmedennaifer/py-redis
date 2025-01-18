199235e6815f16880c1badb80c4c722a5dc9a99c
ahmedennaifer <ahmedennaifer00@gmail.com>
2025-01-18 14:47:11 +0100

    feat: add RDB snapshot binary I/O

--- src/store.py
+++ src/store.py
@@ -11,17 +11,91 @@ else:
 =========================================
 1 - decode the message, then into command result
 2 - store in dict with k=k, v=v 
-3 - return result 
+3 - return result
+ 
+=========     DONE    ==================
+========================================
+=======================================
+=              RDB
+======================================
+1 - convert RESP to binary X
+2 - add crc64 -
+3 - decode back X
+4 - binary file I/O -
 """
 
 class RESPResponse(Enum):
     OK = '+OK'
     ERR = '-ERR'
-    #TODO: status code in err
+    ERR_NOT_FOUND= '-ERR Key not found'
+    ERR_SYNTAX_UNKNOWN = '-ERR Wrong Syntax'
+    ERR_BROKEN_COMMAND = '-ERR Broken command'
+
+    
+# [REDIS][VERSION][DBNUM][KEY-TYPE][KEY][VALUE]...[EOF][CHECK]
+
+class RedisConfig:
+    REDIS_STR = b"REDIS"
+    REDIS_VERSION = bytes([0,1])
+    REDIS_DBNUM = bytes([1])
+    RDB_STRING_TYPE = bytes([0])
+    EOF = bytes([255])
+    
+    def convert_conf_to_binary(self):
+        return b"".join([
+                            self.REDIS_STR,
+                            self.REDIS_VERSION,
+                            self.REDIS_DBNUM,
+                        ])
 class KVStore:
     def __init__(self):
         self.store = {}
 
+
+    def read_from_rdb_snapshot(self, file_path):
+        ...
+    
+    def convert_store_to_binary(self):
+            rs = RedisConfig()
+            rdb = rs.convert_conf_to_binary()
+            for k, v in self.store.items():
+                key_bytes = k.encode('utf-8')
+                if isinstance(v, int):
+                    val_bytes = bytes([v])
+                else:
+                    val_bytes = str(v).encode('utf-8')
+                kv_entry = b"".join([
+                    rs.RDB_STRING_TYPE,
+                    bytes([len(key_bytes)]),
+                    key_bytes,
+                    bytes([len(val_bytes)]),
+                    val_bytes
+                ])
+                rdb += kv_entry
+            rdb += rs.EOF
+            return rdb 
+        
+    def decode_header_from_binary(self, data):
+            return {
+                "magic": data[0:5].decode(),
+                "version": data[5:7],
+                "db": data[7]
+            }
+        
+    def decode_kvs_from_binary(self, data, start=8):
+           result = {}
+           pos = start
+           while pos < len(data) and data[pos] != 255:
+               type_marker = data[pos]
+               key_len = data[pos + 1]
+               key = data[pos + 2:pos + 2 + key_len].decode()
+               val_len = data[pos + 2 + key_len]
+               val = data[pos + 2 + key_len + 1:pos + 2 + key_len + 1 + val_len]
+               result[key] = val.decode() if val_len > 1 else val[0]
+               pos = pos + 2 + key_len + 1 + val_len
+            self.store = result
+                             
+        
     def decode_resp_string(self, resp_string: str) -> Union[CommandResult, RESPResponse]:
         cleaned = resp_string.strip("b'").replace('\\r\\n', '\r\n')
         parts = cleaned.split("\r\n")       
@@ -47,7 +121,6 @@ class KVStore:
             assert instruction in [member.value for member in Command]
             return CommandResult(command=instruction, key=k, value=None)
 
-    
         elif instruction == "DEL":
             n_parts = parts[1]
             instruction = parts[2]
@@ -59,9 +132,9 @@ class KVStore:
 
     def insert(self, parsed_resp: CommandResult):
         if not isinstance(parsed_resp, CommandResult):
-            raise TypeError(f"Argument of type {type(decoded_resp_string)} is not the expected format CommandResult")
+            return RESPResponse.ERR_SYNTAX_UNKNOWN
         if parsed_resp is None:
-            raise TypeError("Expected CommandResult, got None")
+            return RESPResponse.ERR_BROKEN_COMMAND
         try:
             self.store[parsed_resp.key] = parsed_resp.value
             return RESPResponse.OK
@@ -69,25 +142,16 @@ class KVStore:
             print(f"Error inserting: {e}")
               
     def get(self, parsed_resp: CommandResult) -> Union[CommandResult, RESPResponse]:
-        if self.store.get(parsed_resp.key):
-            v = self.store.get(parsed_resp.key)
+        v = self.store.get(parsed_resp.key)
+        if v is not None: 
             return CommandResult(command=Command.RES, key=parsed_resp.key, value=v)
         else:
-            return RESPResponse.ERR        
-        
+            return RESPResponse.ERR_NOT_FOUND
     
     def delete(self, parsed_resp: CommandResult) -> Union[CommandResult, RESPResponse]:
             if self.store.get(parsed_resp.key):
                 self.store.pop(parsed_resp.key)
                 return RESPResponse.OK
             else:
-                return RESPResponse.ERR        
+                return RESPResponse.ERR_NOT_FOUND        
              
-if __name__ == "__main__":
-    kv = KVStore()
-    query = "*3\r\n$3\r\nSET\r\n$4\r\nPoop\r\n$4\r\nPoop\r\n"
-    print(kv.decode_resp_string(query))    
-    test = CommandResult(command=Command.SET, key="1", value="2")
-    print(kv.insert(test))
-    print(kv.store)
-    print(kv.get(test))
